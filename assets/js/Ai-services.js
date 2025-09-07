09e5857fae0472c6bd7bda63527cb920838ef688
Awemick <lemickjac@gmail.com>
2025-09-07 09:56:10 -0700

    Update Ai-services.js

--- assets/js/Ai-services.js
+++ assets/js/Ai-services.js
@@ -1,5 +1,5 @@
 const OCR_SPACE_API_KEY = 'K85308176588957';
-const HUGGING_FACE_API_KEY = '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';
+const HUGGING_FACE_API_KEY = '';
 
 export const ocrService = {
   async extractTextFromImage(imageFile) {
@@ -91,4 +91,5 @@ export const aiService = {
     
     return flashcards;
   }
+
 };
